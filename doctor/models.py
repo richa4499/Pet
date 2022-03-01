98da3f18547f6f7a4da06539243d670a9004fb8f
richa4499 <richapadhariya4499@gmail.com>
2022-03-01 14:15:07 +0530

    fc

---
+++ doctor/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from customer.models import *
+# Create your models here.
---
+++ retailer/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from customer.models import retailer_details
+
+
+# Create your models here.
+
+
